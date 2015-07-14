c4ee4eebeb3a132f1a9b4f05f5330a1581406bbd
Andrew <andrewcrane152@gmail.com>
2015-07-14 14:04:24 -0600

    directive working

--- public/app/directives/directives.js
+++ public/app/directives/directives.js
@@ -1,6 +1,6 @@
-// var app = angular.module('tuneMyPiano'):
-//
-// app.directive('mainHeader', function(){
+var app = angular.module('tuneMyPiano');
+
+// app.directive('introHeader', function(){
 //   return {
 //     restrict: 'E',
 //     templateUrl: "mainHeader.html",
@@ -10,3 +10,37 @@
 //     }
 //   }
 // })
+
+
+app.directive('servicesDescription', function(){
+    function link( $scope, element, attributes) {
+      console.log('inside the function');
+      var expression = attributes.servicesDescription;
+      var duration = (attributes.slideShowDuration || "fast");
+      if (!$scope.$eval(expression)){
+        element.hide();
+      }
+      $scope.$watch(expression, function(newValue, oldValue){
+        if (newValue === oldValue) {
+          return;
+        }
+        if (newValue) {
+          element.stop( true, true ).slideDown(duration);
+        } else {
+          element.stop(true, true).slideUp(duration);
+        }
+      });
+    }
+    return {
+      link: link,
+      restrict: 'E',
+      templateUrl: 'app/directives/servicesDescription.html'
+    };
+});
+
+app.directive('whatToExpect', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/whatToExpect.html'
+  };
+})
